cee7d1a217203a170e4e8d3cdd673942290f3979
Daniel Anyetewen <danyetewen@gmail.com>
2022-06-30 22:16:29 -0700

    low level programming in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main- C program that prints something
+ * Description- you are not allowed to use printf
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
