109672357f3efc2d36e7d1be1e3bc0902b18d72a
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-01 22:58:33 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-	char leter = 'a';
+	char letter = 'a';
 
 	while (letter <= 'z')
 	{
@@ -24,6 +24,6 @@ int main(void)
 		letter++;
 	}
 
-	puchar('\n');
+	putchar('\n');
 	return (0);
 }
