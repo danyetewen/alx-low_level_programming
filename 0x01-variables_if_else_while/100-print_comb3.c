f9852bb52c10487c0b06d1c735b1acc4393eabcf
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-02 08:11:59 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main- writes 00-99
+*Return: zero for success
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; j++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if ((i < j) & (j <= '9'))
+			{
+				putchar(i);
+				putchar(j);
+				if ((j < '9') | (i < '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
