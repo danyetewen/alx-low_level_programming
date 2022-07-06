c6b5e59bc3e56fa676e7ba767d6f7b81293931ef
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-05 22:43:57 -0700

    main work done corrected

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,6 +9,8 @@ void print_alphabet(void)
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
 		_putchar(ch);
+	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,15 +5,20 @@
  *@n: input
 */
 
-void print_to_98(int n);
+void print_to_98(int n)
 {
 	int count;
-
+i
 	if (n > 98)
 		for (count = n; count > 98; --count)
+		{
 			printf("%d, ", count);
+		}
 	else
 		for (count = n; count < 98; ++count)
+		{
 			printf("%d, ", count);
+		}
+
 	printf("98\n");
 }
