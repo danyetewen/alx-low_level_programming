38d823d18917a56ecdf440cb7d9b9ee77c3333c7
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-05 23:08:31 -0700

    main work done corrected

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void print_alphabe(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -14,7 +14,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_o_98(int n);
+void print_of_98(int n);
 void print_times_table(int n);
 
 #endif /* MAIN_H */
