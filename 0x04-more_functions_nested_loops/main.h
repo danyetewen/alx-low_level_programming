235d3d3073fd9595e2adbc19fc19d5cf88fb8c28
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-09 01:53:21 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _isupper(int ic);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 int _putchar(char c);
 void print_numbers(void);
-void print_most_numbers(void;)
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
