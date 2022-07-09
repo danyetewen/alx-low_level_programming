bd3c6b80d75515e19947d3eb5c2bd38799f1afef
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-09 04:50:13 -0700

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include  "main.h"
 
 /**
- * print_diagonal - print a diagonal line 
+ * print_diagonal - print a diagonal line
  * @n: is the number of times the \ character should be printed
 */
 
