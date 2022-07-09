5cc33168996d46256ddf48b478958b52314a6f49
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-09 02:38:25 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - print 0 - 9 apart from 2 and 4 and you
- * 		can only use _putchar twice
+ *		can only use _putchar twice
  * Return: always 0 (success)
 */
 void print_most_numbers(void)
