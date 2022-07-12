72197403996f44bda940c395ab3fa2d3f8aadb26
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-11 21:31:36 -0700

    a function that convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,7 @@
 /**
  * _atoi - converts a string to an integer
  *@s: string input parameter
-Return: converted integer from string 
+*Return: converted integer from string
 */
 
 int _atoi(char *s)
