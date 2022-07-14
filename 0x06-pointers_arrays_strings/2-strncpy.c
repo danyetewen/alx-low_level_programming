2d639687312f755127a234512dad59199131a338
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-13 22:33:03 -0700

    points arrays and strings corrected

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	{
+	strncpy(dest, src, n)
 
 	return (dest);
 }
