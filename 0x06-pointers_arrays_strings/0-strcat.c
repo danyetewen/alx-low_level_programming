4990944812f5f2ca0cd7281e602b10261482888e
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-13 19:38:07 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - function tht concatenates two strings.
+ * @dest : pointer to destination input
+ * @src : pointer to resulting string @dest
+ * Return: pointer to resulting string @dest
+*/
+
+char *_strcat(char *des, char *src)
+{
+	int c = 0, c2;
+
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2]; c2++)
+		dest[c++] = src[c2];
+
+	return (dest);
+}
+	
+
+
+
