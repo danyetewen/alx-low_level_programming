02b2acc3bde104ee0ba62f5026d171c333225fca
Daniel Anyetewen <danyetewen@gmail.com>
2022-07-18 21:21:32 -0700

    a function that locates a substring

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum ot the two diagonals
+ * of a square matrix of integers.
+ * @a: input pointer
+ * @size: size of he matrix
+ * Return: nothing
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1, s2;
+
+	s1 = 0;
+	s2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+
+		if (i % (size + 1) == 0)
+			s1 += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			s2 += a[i];
+	}
+	printf("%d, %d\n", s1, s2);
+}
