09f696c865dcdc386e931c08281270601c2d417a
Daniel Anyetewen <danyetewen@gmail.com>
2022-08-01 13:37:05 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  * Description: prints the name of the file it was
- * compiled from, followed by a new line 
+ * compiled from, followed by a new line
  * Return: Always 0 (success)
 */
-
+i
 int main(void)
 {
 	printf("%s/n", __FILE__);
