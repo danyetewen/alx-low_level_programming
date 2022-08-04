2b423ce7ec44fd242417efd709d023c179774bb0
Daniel Anyetewen <danyetewen@gmail.com>
2022-08-03 17:58:18 -0700

    a function that searches for an integer.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,9 +3,23 @@
 
 #include <stddef.h>
 #include <stdio.h>
+
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1 
+ * FALSE: 0
+*/
+
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
 
 #endif 
 
